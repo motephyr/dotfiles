e0ae5fa89ec7496b3139abc4d1cb6a4642ff0b1c
motephyr <motephyr@gmail.com>
2020-10-29 12:11:26 +0800

    Fix

--- link-nvim.sh
+++ link-nvim.sh
@@ -11,13 +11,13 @@ ln -s $DOTFILES/vimrc .vimrc
 
 echo "Files are linked."
 
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Installed vim-plug for neovim"
 
 echo "=================================================="
 echo "Need install rg. check https://github.com/BurntSushi/ripgrep#installation"
-echo "Add alias vi=\"~/nvim.appimage\" to .bashrc or .zshrc"
+echo "Add alias vi=\"~/nvim.appimage\" to ~/.bashrc or ~/.zshrc"
 echo "source ~/.bashrc"
 echo "=================================================="
