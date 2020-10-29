3e483ee8db1f2e7b0b01eb264c7f1941ad2c29db
motephyr <motephyr@gmail.com>
2020-10-29 12:35:30 +0800

    Fix

--- link-nvim.sh
+++ link-nvim.sh
@@ -7,13 +7,11 @@ echo "Linking .vimrc & install vim-plug"
 
 mv ~/.vimrc $DOTFILES/$BACKUPFILES/vimrc.bk
 
-ln -s $DOTFILES/vimrc ~/.vimrc
+ln -s $DOTFILES/.config/nvim/init.vim ~/.vimrc
 
 echo "Files are linked."
 
-mkdir -p ~/.vim/autoload
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+sh -c 'curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Installed vim-plug for neovim"
